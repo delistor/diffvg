22cbb146f701af0d1a355db014e76fbe40e7c292
Desolade <54382349+delistor@users.noreply.github.com>
2025-05-02 17:14:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,65 +1,63 @@
 # Adapted from https://github.com/pybind/cmake_example/blob/master/setup.py
 import os
-import re
 import sys
 import platform
 import subprocess
 import importlib
 from sysconfig import get_paths
-
-import importlib
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from setuptools.command.install import install
 from distutils.sysconfig import get_config_var
-from distutils.version import LooseVersion
+import pybind11
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir, build_with_cuda):
-        Extension.__init__(self, name, sources=[])
+        super().__init__(name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
         self.build_with_cuda = build_with_cuda
 
 class Build(build_ext):
     def run(self):
         try:
-            out = subprocess.check_output(['cmake', '--version'])
+            subprocess.check_output(['cmake', '--version'])
         except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
+            raise RuntimeError("CMake must be installed")
 
         super().run()
 
     def build_extension(self, ext):
         if isinstance(ext, CMakeExtension):
             extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-            info = get_paths()
-            include_path = info['include']
-            cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                          '-DPYTHON_LIBRARY=' + get_config_var('LIBDIR'),
-                          '-DPYTHON_INCLUDE_PATH=' + include_path]
+            include_path = get_paths()["include"]
+            pybind11_include = pybind11.get_include()
+
+            cmake_args = [
+                f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
+                f"-DPYTHON_LIBRARY={get_config_var('LIBDIR')}",
+                f"-DPYTHON_INCLUDE_PATH={include_path}",
+                f"-DPYBIND11_INCLUDE_DIR={pybind11_include}",
+                f"-DDIFFVG_CUDA={'1' if ext.build_with_cuda else '0'}"
+            ]
 
             cfg = 'Debug' if self.debug else 'Release'
             build_args = ['--config', cfg]
 
             if platform.system() == "Windows":
-                cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir),
-                               '-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+                cmake_args += [
+                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}",
+                    f"-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
+                ]
                 if sys.maxsize > 2**32:
                     cmake_args += ['-A', 'x64']
                 build_args += ['--', '/m']
             else:
-                cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+                cmake_args += [f"-DCMAKE_BUILD_TYPE={cfg}"]
                 build_args += ['--', '-j8']
 
-            if ext.build_with_cuda:
-                cmake_args += ['-DDIFFVG_CUDA=1']
-            else:
-                cmake_args += ['-DDIFFVG_CUDA=0']
-
             env = os.environ.copy()
-            env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                                  self.distribution.get_version())
+            env['CXXFLAGS'] = f'{env.get("CXXFLAGS", "")} -DVERSION_INFO="{self.distribution.get_version()}"'
+
             if not os.path.exists(self.build_temp):
                 os.makedirs(self.build_temp)
             subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
@@ -67,33 +65,39 @@ class Build(build_ext):
         else:
             super().build_extension(ext)
 
+# === 环境检测：是否需要 CUDA ===
 torch_spec = importlib.util.find_spec("torch")
 tf_spec = importlib.util.find_spec("tensorflow")
 packages = []
 build_with_cuda = False
-if torch_spec is not None:
+
+if torch_spec:
     packages.append('pydiffvg')
     import torch
     if torch.cuda.is_available():
         build_with_cuda = True
-if tf_spec is not None and sys.platform != 'win32':
+if tf_spec and sys.platform != 'win32':
     packages.append('pydiffvg_tensorflow')
     if not build_with_cuda:
         import tensorflow as tf
-        if tf.test.is_gpu_available(cuda_only=True, min_cuda_compute_capability=None):
-            build_with_cuda = True
-if len(packages) == 0:
-    print('Error: PyTorch or Tensorflow must be installed. For Windows platform only PyTorch is supported.')
-    exit()
-# Override build_with_cuda with environment variable
+        if hasattr(tf.test, "is_gpu_available"):
+            build_with_cuda = tf.test.is_gpu_available(cuda_only=True)
+if not packages:
+    print("❌ Error: PyTorch or TensorFlow must be installed.")
+    exit(1)
+
+# 🔧 环境变量控制
 if 'DIFFVG_CUDA' in os.environ:
     build_with_cuda = os.environ['DIFFVG_CUDA'] == '1'
 
-setup(name = 'diffvg',
-      version = '0.0.1',
-      install_requires = ["svgpathtools"],
-      description = 'Differentiable Vector Graphics',
-      ext_modules = [CMakeExtension('diffvg', '', build_with_cuda)],
-      cmdclass = dict(build_ext=Build, install=install),
-      packages = packages,
-      zip_safe = False)
+setup(
+    name='diffvg',
+    version='0.0.1',
+    install_requires=["svgpathtools"],
+    description='Differentiable Vector Graphics',
+    ext_modules=[CMakeExtension('diffvg', '', build_with_cuda)],
+    cmdclass=dict(build_ext=Build, install=install),
+    packages=packages,
+    zip_safe=False,
+)
+
